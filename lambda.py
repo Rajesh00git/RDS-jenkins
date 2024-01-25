bebf2bb9abcac95c87116ea094e596b857981386
Rajesh00git <136963194+Rajesh00git@users.noreply.github.com>
2024-01-25 15:33:06 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -3,7 +3,7 @@ lambda_Client = boto3.client('lambda', region_name="ap-south-1")
 response = lambda_Client.create_function(
     Code={
         'S3Bucket': 'lambda-python-jen',
-        'S3Key': 'test.zip',  # how can i create or fetch this S3Key
+        'S3Key': 'test.py',  # how can i create or fetch this S3Key
     },
 
     FunctionName='s3-lambda-ec2',
